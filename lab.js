961fb2d759e48a0cfa35ac01a5cb854994a337d5
ArchimedesD <addickso@ucsc.edu>
2024-05-25 13:20:31 -0700

    button FIX

--- lab.js
+++ lab.js
@@ -1,8 +1,5 @@
-document.getElementById('recycleButton').addEventListener('click', function() {
-    var paragraph = document.getElementById('recycleText');
-    if (paragraph.style.display === 'none') {
-        paragraph.style.display = 'block';
-    } else {
-        paragraph.style.display = 'none';
-    }
+$(document).ready(function() {
+    $('#recycleButton').on('click', function() {
+        $('#recycleText').toggle(); // Toggle the visibility of the paragraph
+    });
 });
