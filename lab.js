9e8239a9c2de2dba2b7e65117f07dc60aa27bca9
kikic777 <166079146+kikic777@users.noreply.github.com>
2024-06-06 17:38:54 -0700

    Reformatted js file

--- lab.js
+++ lab.js
@@ -1,67 +1,54 @@
-// Individual Buttons
+// Buttons
 
 $(document).ready(function() {
     $('#recycleButton').on('click', function() {
         $('#recycleText').slideToggle('slow'); // Toggle the visibility of the paragraph
     });
-});
 
-$(document).ready(function() {
     $('#waterButton').on('click', function() {
         $('#waterText').slideToggle('slow');
     });
-});
 
-$(document).ready(function() {
     $('#postButton').on('click', function() {
         $(".hidey-bit").hide();
         $('#postText').slideToggle('slow');
     });
-});
 
-$(document).ready(function() {
+    // $('#postButton').on('click', function() {
+    //     if ($('#postText').is(':visible')) {
+    //         $('#postText').slideUp('slow');
+    //     } else {
+    //         $(".hidey-bit").slideUp('slow'); // Hide all sections with animation
+    //         $('#postText').slideDown('slow');
+    //     }
+    // });
+
     $('#susButton').on('click', function() {
         $(".hidey-bit").hide();
         $('#susText').slideToggle('slow');
     });
-});
 
-$(document).ready(function() {
     $('#energyButton').on('click', function() {
         $('#energyText').slideToggle('slow');
     });
-});
 
-// Institutional Buttons
-
-$(document).ready(function() {
     $('#voteButton').on('click', function() {
         $('#voteText').slideToggle('slow');
     });
-});
 
-$(document).ready(function() {
     $('#boyButton').on('click', function() {
         $('#boyText').slideToggle('slow');
     });
-});
 
-$(document).ready(function() {
     $('#orgButton').on('click', function() {
         $('#orgText').slideToggle('slow');
     });
-});
 
-$(document).ready(function() {
     $('#repButton').on('click', function() {
         $('#repText').slideToggle('slow');
     });
-});
 
-$(document).ready(function() {
     $('#localButton').on('click', function() {
         $('#localText').slideToggle('slow');
     });
 });
-
-$(".hidey-bit").hide();
